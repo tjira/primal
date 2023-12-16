ca362d53d9c26417815acca7c2a3b4568cf5502c
Tomas Jira <tom.jira@gmail.com>
2023-12-16 20:42:37 +0100

    fix compilation bug

--- include/sieve.h
+++ include/sieve.h
@@ -3,14 +3,10 @@
 #include <gmpxx.h>
 #include <vector>
 
-#define SIZE 100
-
 class Sieve {
 public:
     static mpz_class nextPrime(mpz_class n);
     static bool isMersenne(mpz_class p);
     static mpz_class nextMersenne(mpz_class p);
     static bool isPrime(mpz_class n);
-
-    constexpr inline static std::vector<mpz_class> sieve{};
 };
